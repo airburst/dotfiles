946d440481fe32dc349598878bdb2ae13f907e79
Mark Fairhurst <mark@fairhursts.net>
2024-03-31 15:28:29 +0100

    feat: Set correct permissions on rsa file

--- .ssh/decrypt.sh
+++ .ssh/decrypt.sh
@@ -3,4 +3,7 @@
 # Decrypts the id_rsa.enc file with the given password
 openssl aes-256-cbc -d -a -pbkdf2 -in id_rsa.enc -out id_rsa -pass pass:$1
 
+# Set permissions
+chmod 600 id_rsa
+
 # rm id_rsa.enc
